1b08717c5f43cd3d6a8dfb29d6b4f60633fe4d14
imran8r <imranhrrabbi@gmail.com>
2025-08-22 01:47:46 +0600

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,19 @@
+a='Quick Brown fox jumps over the lazy fox'
+b=(a.upper())
+print (b)
+c=print(b[6:16])
+
+X='ABRACADABRA'
+print(X[2])
+
+A= 'Amar'
+B='Shonar'
+C='Bangla'
+D=A+' '+B+' '+C
+print(D)
+
+
+Singer_Name=input('Who is your favourite singer?')
+Song_Name= input('Put your favourite song')
+H= f'my favourite singer is {Singer_Name}, My favourite song is {Song_Name}'
+print(H)
